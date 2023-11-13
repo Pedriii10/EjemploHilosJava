1628612dbaf681be0df1e16bdd66bb7c1936fd70
Pedriii10 <pedromonzon239@gmail.com>
2023-11-13 09:48:14 +0100

    Ejercicio 1 terminado

--- app/src/main/java/com/example/ejemplohilos/MainActivity.java
+++ app/src/main/java/com/example/ejemplohilos/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.ejemplohilos;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-
-//Metodo AsynTask
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvCrono;
+    private Button startButton, stopButton, restartButton;
+    private CountDownTask countDownTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +19,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvCrono = findViewById(R.id.tvCrono);
-        new CountDownTask().execute(10);
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        restartButton = findViewById(R.id.restartButton);
+
+        startButton.setOnClickListener(v -> startOrResumeTask());
+        stopButton.setOnClickListener(v -> pauseTask());
+        restartButton.setOnClickListener(v -> restartTask());
+    }
+
+    private void startOrResumeTask() {
+        if (countDownTask == null || countDownTask.getStatus() == AsyncTask.Status.FINISHED) {
+            countDownTask = new CountDownTask();
+            countDownTask.execute(10);
+        } else {
+            countDownTask.resumeCounting();
+        }
+    }
+
+    private void pauseTask() {
+        if (countDownTask != null && countDownTask.getStatus() == AsyncTask.Status.RUNNING) {
+            countDownTask.pauseCounting();
+        }
+    }
+
+    private void restartTask() {
+        if (countDownTask != null) {
+            if (countDownTask.getStatus() == AsyncTask.Status.RUNNING) {
+                countDownTask.cancel(true);
+            }
+        }
+        countDownTask = new CountDownTask();
+        countDownTask.execute(10);
+        countDownTask.resumeCounting();
+    }
+
+
+    private int getCurrentRemainingValue() {
+        String currentText = tvCrono.getText().toString();
+        try {
+            return Integer.parseInt(currentText);
+        } catch (NumberFormatException e) {
+            return 10;
+        }
     }
 
     private class CountDownTask extends AsyncTask<Integer, Integer, String> {
 
+        private boolean countingPaused = false;
+        private final Object pauseLock = new Object();
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            tvCrono.setText("Comenzamos");
         }
 
         @Override
         protected String doInBackground(Integer... params) {
             int remaining = params[0];
-            while (remaining > 0) {
+            while (remaining > 0 && !isCancelled()) {
+                synchronized (pauseLock) {
+                    while (countingPaused) {
+                        try {
+                            pauseLock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
                 publishProgress(remaining);
                 remaining--;
 
@@ -41,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+
             return "Terminado";
         }
 
@@ -54,6 +110,17 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             tvCrono.setText(result);
+        }
+
+        public void pauseCounting() {
+            countingPaused = true;
+        }
+
+        public void resumeCounting() {
+            countingPaused = false;
+            synchronized (pauseLock) {
+                pauseLock.notifyAll();
+            }
         }
     }
 }
