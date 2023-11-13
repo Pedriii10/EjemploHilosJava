d422eea05b2d2d18be4277e2d2e435a34eec05eb
Pedriii10 <pedromonzon239@gmail.com>
2023-11-13 09:13:06 +0100

    Explicacion de lo que pasa en el primer codigo

--- app/src/main/java/com/example/ejemplohilos/MainActivity.java
+++ app/src/main/java/com/example/ejemplohilos/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.ejemplohilos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+
+// Al ejecutar este codigo, la interfaz de usuario queda bloqueada hasta que el bucle termine
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvCrono = (TextView)findViewById(R.id.tvCrono);
+        int remaining = 10;
+        tvCrono.setText("Comenzamos");
+        while(remaining > 0) {
+            tvCrono.setText("" + remaining); remaining--;
+            try {
+                Thread.sleep(1000);
+            }catch(InterruptedException e){}
+        }
+        tvCrono.setText("Terminado");
     }
 }
